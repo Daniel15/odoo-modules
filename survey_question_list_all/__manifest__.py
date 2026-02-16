920a00a06dd7a4b1933e845d33155923ab4845fd
Daniel Lo Nigro <git@d.sb>
2026-02-16 14:26:33 -0800

    [18.0][ADD] survey_question_list_all: New module

---
+++ survey_question_list_all/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Daniel Lo Nigro
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "List All Survey Questions",
+    "summary": "List all questions in the survey form instead of only 40 per page",
+    "version": "18.0.1.0.0",
+    "category": "Marketing",
+    "depends": ["survey"],
+    "data": ["views/survey_survey_views.xml"],
+    "author": "Daniel Lo Nigro",
+    "maintainers": ["Daniel15"],
+    "website": "https://d.sb/odoo-modules",
+    "license": "AGPL-3",
+    "installable": True,
+}
