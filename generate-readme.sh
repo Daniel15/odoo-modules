4d18d7018289e741c4db052b898ae51731aa2297
Daniel Lo Nigro <git@d.sb>
2026-02-16 15:19:42 -0800

    Strip OCA banner from index.html files, since this isn't an official OCA project.

--- generate-readme.sh
+++ generate-readme.sh
@@ -4,3 +4,6 @@ oca-gen-addon-readme --org-name Daniel15 --repo-name odoo-modules --branch 18.0 
 
 # Strip OCA banner image from generated README files
 find . -path '*/README.rst' -exec sed -i '/^.. image:: https:\/\/odoo-community.org\/readme-banner-image$/,/^$/d' {} \;
+
+# Strip OCA banner image from generated index.html files
+find . -path '*/static/description/index.html' -exec sed -i '/<a class="reference external image-reference" href="https:\/\/odoo-community.org\/get-involved/,/<\/a>/d' {} \;
