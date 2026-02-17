d49b040fe9bd85b55cd1e39111e1a5d887d38b51
Daniel Lo Nigro <git@d.sb>
2026-02-16 16:19:37 -0800

    Exclude docker directory in generate-readme

--- generate-readme.sh
+++ generate-readme.sh
@@ -3,7 +3,7 @@ oca-gen-addons-table
 oca-gen-addon-readme --org-name Daniel15 --repo-name odoo-modules --branch 18.0 --addons-dir .
 
 # Strip OCA banner image from generated README files
-find . -path '*/README.rst' -exec sed -i '/^.. image:: https:\/\/odoo-community.org\/readme-banner-image$/,/^$/d' {} \;
+find . -path './docker' -prune -o -path '*/README.rst' -exec sed -i '/^.. image:: https:\/\/odoo-community.org\/readme-banner-image$/,/^$/d' {} \;
 
 # Strip OCA banner image from generated index.html files
-find . -path '*/static/description/index.html' -exec sed -i '/<a class="reference external image-reference" href="https:\/\/odoo-community.org\/get-involved/,/<\/a>/d' {} \;
+find . -path './docker' -prune -o -path '*/static/description/index.html' -exec sed -i '/<a class="reference external image-reference" href="https:\/\/odoo-community.org\/get-involved/,/<\/a>/d' {} \;
